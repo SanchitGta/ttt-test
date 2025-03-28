635914849d5245e0c8b215b70e7ff4fcb0411840
SanchitGta <sanchit.gupta@think41.com>
2025-03-28 15:16:36 +0530

    Auto-commit changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UsernameForm from './UsernameForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Username Submission</h1>
+      <UsernameForm />
+    </div>
+  );
+};
+
+export default App;
